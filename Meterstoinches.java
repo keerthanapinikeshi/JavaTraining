41ec9c8e10e13db40ddbd15a1f18217c0d87bf4c
keerthanapinikeshi <90104373+keerthanapinikeshi@users.noreply.github.com>
2022-12-16 15:40:00 -0600

    Create Meterstoinches.java

---
+++ Meterstoinches.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        double inches=1000;
+        // Convert inches to meters
+        double meters = inches * 0.0254;
+
+        // Display result
+        System.out.println(inches + " inches is " + meters + " meters");
+    }
+    }
