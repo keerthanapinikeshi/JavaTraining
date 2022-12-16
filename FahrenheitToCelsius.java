dadd28f419483f400251b9c76ddd9f32aa769ff2
keerthanapinikeshi <90104373+keerthanapinikeshi@users.noreply.github.com>
2022-12-16 15:40:39 -0600

    Create FahrenheitToCelsius.java

---
+++ FahrenheitToCelsius.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class FahrenheitToCelsius
+{
+    public static void main(String[] args) {
+        float f = 212;
+        float c;
+        c = (f - 32) * 5/9;
+        System.out.println("Temperature in Celsius : " + c);
+
+    }
+}
